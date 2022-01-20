48a1d486628c996b808e9d09d34cd3737c88e70f
Koro <youcry987951@gmail.com>
2022-01-20 18:53:27 +0900

    finish join and leave function

---
+++ bot.py
@@ -0,0 +1,25 @@
+from operator import truediv
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='|', intents = intents)
+
+@bot.event
+async def on_ready():
+    print(">> Bot is Online <<")
+    
+@bot.event
+async def on_member_join(member):
+    print(f'{member} join!')
+    channel = bot.get_channel(933657649713647627)
+    await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} leave!')
+    channel = bot.get_channel(933657680751493181)
+    await channel.send(f'{member} leave!')
+    
+bot.run('OTMzNjIzOTMzOTYwMzM1NDAw.YekPCw.jp2-fgGv7MIGtVK2L0qx92munJ4')
